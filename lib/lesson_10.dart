430cc7293da7ca1e130447a3316c85eabe988b65
olegpetskovich <OlegPetskovich@gmail.com>
2023-11-04 21:15:38 +0200

    Oleg code

--- lib/lesson_10.dart
+++ lib/lesson_10.dart
@@ -1,3 +1,9 @@
-void main(){
-  print("Hello");
+void main() {
+  for (int i = 0; i < 10; i++) {
+    if (i % 2 == 0)
+      print("Ilyuha TOP MAN");
+    else {
+      print("Ilyuha SUPER MAN");
+    }
+  }
 }
