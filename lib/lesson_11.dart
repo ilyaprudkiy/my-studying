3cc6b4c9e034772f1633cc841a932c192d17c1f0
ilyaprudkiy <146878764+ilyaprudkiy@users.noreply.github.com>
2023-11-06 11:10:25 +0300

    constructor

---
+++ lib/lesson_11.dart
@@ -0,0 +1,18 @@
+//Конструктор
+class Rabotnik{
+  final String name;
+  final List<int> zp;
+  Rabotnik ({required this.name, required this.zp}){
+   // print('Создан');
+    int result=0;
+    for(var i=0; i<zp.length;i++) {
+      result=zp[i]+result;
+    }
+    print(result/zp.length);// УЗНАЛ СРЕДНЮЮ ЗП РАБОТНИКА
+}
+
+}
+void main(){
+  var ivan = Rabotnik(name:'Ivan',zp: [45000,38000,47000]);
+  var petr = Rabotnik(name:'Petr',zp: [31000,28000,45000]);
+}
