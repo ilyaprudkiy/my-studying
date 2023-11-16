c7fe8694ded9ae133b7b9b983b35066c21f1cfa4
olegpetskovich <OlegPetskovich@gmail.com>
2023-11-16 11:17:42 +0200

    A little clean code

--- lib/lesson_12.dart
+++ lib/lesson_12.dart
@@ -1,26 +1,28 @@
 //геттеры и сеттеры
 
-class Rabotnik{
+class Rabotnik {
   String name;
-  List<int>zp;
+  List<int> zp;
 
-  Rabotnik(this.name,this.zp);
-  double get sreedZp{
-    var rezultat=0;
-    for(var i=0; i<zp.length;i++){
-      rezultat=rezultat+zp[i];
+  Rabotnik(this.name, this.zp);
+
+  double get sreedZp {
+    var rezultat = 0;
+    for (var i = 0; i < zp.length; i++) {
+      rezultat = rezultat + zp[i];
     }
-    return rezultat / zp.length ;
+    return rezultat / zp.length;
   }
-  set dopOplata(value){
+
+  set dopOplata(value) {
     zp.add(value);
   }
-  }
+}
 
-void main () {
+void main() {
   var ivan = Rabotnik('Ivan', [23000, 27000, 43000]);
   var petr = Rabotnik('Petr', [27000, 33000, 47000]);
-print(ivan.zp);
-  ivan.dopOplata=7500;
+  print(ivan.zp);
+  ivan.dopOplata = 7500;
   print(ivan.zp);
 }
