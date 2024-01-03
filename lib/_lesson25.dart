c6945c6ee1d5b83519e79c2fcf66f50cb6dc3a31
ilyaprudkiy <ilya.prudkiy20@gmail.com>
2024-01-03 14:42:49 +0200

    Implements, abstract class.

--- lib/_lesson25.dart
+++ lib/_lesson25.dart
@@ -1,21 +1,114 @@
-abstract class Printtable {
-  void info();
+// abstract class Printtable {
+//   void info();
+// }
+//
+// class Book implements Printtable {
+//   String author;
+//   String name;
+//
+//   Book(this.author, this.name);
+//
+//   @override
+//   String info() {
+//     return '$author , $name';
+//   }
+// }
+//
+// void main(){
+//   var myBook = Book('J.K Rouling', 'Harry Potter');
+//
+//   print(myBook.info());
+// }
+// abstract class Shape {
+//   double calculatePerimetr();
+//
+//   double calculeteArea();
+// }
+//
+// class Rectangle implements Shape {
+//   double side1;
+//   double side2;
+//   double side3;
+//   double side4;
+//   double width;
+//   double heigth;
+//
+//   Rectangle(this.side1, this.side2, this.side3, this.side4);
+//
+//   @override
+//   double calculatePirimetr() {
+//     return side1 + side2 + side3 + side4;
+//   }
+//
+//   double calculateArea(){
+//
+//   }
+// }
+//
+// class Circle implements Shape {
+//   double radius;
+//
+//   Circle(this.radius);
+//
+//   @override
+//   double calculateArea() {
+//     return 3.14 * radius;
+//   }
+// }
+abstract class Vehicle {
+  void start();
+  void stop();
+
 }
 
-class Book implements Printtable {
-  String author;
-  String name;
+class Car implements Vehicle {
+  bool startCar;
+  int speedCar;
 
-  Book(this.author, this.name);
+  Car(this.startCar, this.speedCar);
 
   @override
-  String info() {
-    return '$author , $name';
+  void start() {
+    if (startCar == true) {
+      print('Машина завелась');
+    } else if (speedCar > 0 && speedCar <= 100) {
+      print('Машина набирает скорость');
+    } else {
+      print('Машина не завелась, попробуйте ёще раз');
+    }
+  }
+
+  @override
+  void stop() {
+    if (startCar == true) {
+      print('Машина остановилась');
+    } else if (speedCar > 100) {
+      print('Машина остановилась , нарушение скростных ограничений');
+    } else {
+      print('Продолжаем движение');
+    }
+  }
+}
+
+class Motorcycle implements Vehicle {
+  @override
+  String start(){
+    return 'Мотоцикл завелся';
+  }
+  @override
+  String  stop(){
+    return 'Мотоцикл остановился';
   }
 }
 
 void main(){
-  var myBook = Book('J.K Rouling', 'Harry Potter');
+  var car = Car(true, 120);
+  var motorcycle = Motorcycle();
 
-  print(myBook.info());
+  car.start();
+  car.stop();
+
+ print(motorcycle.start());
+  print(motorcycle.stop());
 }
+
