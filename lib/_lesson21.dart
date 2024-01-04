4d9a18305d4c5e2f077bd2d7f8a3c2f10c4f4a86
olegpetskovich <OlegPetskovich@gmail.com>
2024-01-04 17:38:55 +0200

    Clean code

--- lib/_lesson21.dart
+++ lib/_lesson21.dart
@@ -37,39 +37,34 @@ class Rectangle {
   int side2;
   int side3;
 
-
   Rectangle(
     this.width,
     this.height,
     this.side1,
     this.side2,
     this.side3,
-
   );
 
-   get info {
+  get info {
     print(width);
     print(height);
   }
 
-set side4(value) {
+  set side4(value) {}
 
-}
-
-  int Show() {
+  int show() {
     return height * width;
   }
 
   int showPerimetr() {
     return side1 + side2 + side3;
   }
-
 }
 
 void main() {
-  var a = Rectangle(20, 40, 10, 4, 10 );
+  var a = Rectangle(20, 40, 10, 4, 10);
   print(a.showPerimetr());
-  print(a.Show());
+  print(a.show());
   a.info;
   a.side4 = 42;
 }
